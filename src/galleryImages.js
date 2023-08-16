59446ea32880eb89eb197bc89536bef0ea872332
Roma Antoliak <104556918+roma-cwby@users.noreply.github.com>
2023-08-16 19:47:01 +0300

    upd gallery img

--- src/galleryImages.js
+++ src/galleryImages.js
@@ -1,6 +1,6 @@
 import i1 from './img/gallery/1.jpg';
 import i2 from './img/gallery/2.jpg';
-import i3 from './img/gallery/3.jpg';
+// import i3 from './img/gallery/3.jpg';
 // import i4 from './img/gallery/4.jpg';
 import i5 from './img/gallery/5.jpg';
 // import i6 from './img/gallery/6.jpg';
@@ -14,7 +14,7 @@ import i13 from './img/gallery/13.jpg';
 import i14 from './img/gallery/14.jpg';
 import i15 from './img/gallery/15.jpg';
 // import i16 from './img/gallery/16.jpg';
-import i17 from './img/gallery/17.jpg';
+// import i17 from './img/gallery/17.jpg';
 import i18 from './img/gallery/18.jpg';
 // import i19 from './img/gallery/19.jpg';
 // import i20 from './img/gallery/20.jpg';
@@ -26,34 +26,51 @@ import i23 from './img/gallery/23.jpg';
 // import i26 from './img/gallery/26.jpg';
 // import i30 from './img/gallery/30.jpg';
 // import i31 from './img/gallery/31.jpg';
-import i32 from './img/gallery/32.jpg';
+// import i32 from './img/gallery/32.jpg';
 // import i33 from './img/gallery/33.jpg';
 // import i34 from './img/gallery/34.jpg';
-import i35 from './img/gallery/35.jpg';
+// import i35 from './img/gallery/35.jpg';
 import i36 from './img/gallery/36.jpg';
 // import i37 from './img/gallery/37.jpg';
 import i40 from './img/gallery/40.jpg';
 import i43 from './img/gallery/43.jpg';
+import i50 from './img/gallery/50.jpg';
+import i51 from './img/gallery/51.jpg';
+import i52 from './img/gallery/52.jpg';
+import i53 from './img/gallery/53.jpg';
+import i54 from './img/gallery/54.jpg';
+import i55 from './img/gallery/55.jpg';
+import i56 from './img/gallery/56.jpg';
+import i57 from './img/gallery/57.jpg';
+import i59 from './img/gallery/59.jpg';
+import i60 from './img/gallery/60.jpg';
+import i61 from './img/gallery/61.jpg';
 
 export const imgs = [
   i1,
+  i50,
   i13,
   i2,
   i43,
-  i35,
   i22,
-  i3,
+  i54,
+
+  i51,
   i9,
   i23,
-  i17,
+  i53,
   i8,
-  i32,
   i15,
+  i56,
   i40,
+  i60,
   i21,
+  i55,
   i36,
+  i61,
   i5,
   i14,
   i18,
+  i57,
   i12,
 ];
